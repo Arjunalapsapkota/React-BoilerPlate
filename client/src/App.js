c9ea39cd1950c58654164bf45a94a69ef1d17827
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-03-09 17:34:06 -0800

    set package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App jumbotron">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Boiler Plate</h2>
+          <small>You can clone this template to deploy on Heroku</small>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
